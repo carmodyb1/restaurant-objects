993db5fac4686b7de69ea37c1ae8edf62ce62909
Brad Carmody <brad.carmody@hotmail>
2014-07-07 11:43:25 -0600

    Problem 1 done

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+var FoodItem = function(name, calories, vegan, glutenFree, citrusFree) {
+  this.name = name;
+  this.calories = calories;
+  this.vegan = vegan;
+  this.glutenFree = glutenFree;
+  this.citrusFree = citrusFree;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $(document).on('ready', function() {
   
 });
