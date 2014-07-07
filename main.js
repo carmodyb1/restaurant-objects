0a206e3d7b2ca52794922022456d9c12b6db7b9c
lc00 <leannecheung00@gmail.com>
2014-07-07 12:02:38 -0600

    finished part I sec.2

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ var FoodItem = function(name, calories, vegan, glutenFree, citrusFree) {
   this.vegan = vegan;
   this.glutenFree = glutenFree;
   this.citrusFree = citrusFree;
+  this.toString = function(){
+  	return "name: " + this.name + "\n" +
+  				 "colories: " + this.calories +   "\n" +
+  				 "vegan: " + this.vegan +  "\n" +
+  				 "gluten_free: " + this.glutenFree + "\n" +
+  				 "citrus_free: " + this.citrusFree;
+  }
 };
 
 
